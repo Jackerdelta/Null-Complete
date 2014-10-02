1aedff6e7f38e0eded533a8784306443def1e13d
Jackerdelta <jackerdelta@gmail.com>
2014-10-02 17:43:10 -0400

    started to update Game.py & started shop.

--- main/Game/Game.py
+++ main/Game/Game.py
@@ -19,28 +19,42 @@ class main():
     
         self.print_score=0
         
+        self.classInstance=shop(self.user,self.points.getPoints(self.user,False)
+        
         st_=raw_input("Start? Y/N:")
         if st_.lower()=="yes" or "y":
+            print("Wait there a second!, we to configure some stuff first.")
+            time_to_show_score=raw_input("# of turns before score is shown: ")
             this.main_loop(self)
         else:
             print("Well thanks for coming along for the party!")
             sys.exit("Bye bye!")
+            
+            
+            
     def main_loop(self):
         active_state=True
         while active_state==True:
             #Start loop
             
             prim=raw_input("Pick Apple? Y/N: ")
-            print(prim)
+            
             if prim.lower()=="yes" or "y" or "Y":
                 self.points.addPoint(self.scoreLVL1, False)
                 self.print_score+=1
-                print("hi")
                 
-                if self.print_score==10:
+                if self.print_score==time_to_show_score:
                     self.points.getPoints(self.user, True)
+                    time_to_show_score=0
                 active_state=True
                 
+            if prim.lower()=="no" or "n" or "N":
+                print("Thanks for playing.")
+                sys.exit("Really, thanks!")
+            if prim.lower()=="shop" or "s" or "S":
+                
+            else:
+                print("Invalid Command.")
                 
         self.points.addPoint(10, True)
         #Debugging Thing: points.getPoints(user,True)
